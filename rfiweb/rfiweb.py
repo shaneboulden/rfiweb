ec4adc6deb411bf6447afcb9b7e854e2c5e89145
jockey10 <shane.boulden@gmail.com>
2019-01-08 21:21:11 +1100

    refactor routes

--- rfiweb/rfiweb.py
+++ rfiweb/rfiweb.py
@@ -29,27 +29,25 @@ def index():
 # POST methods
 @application.route('/rfisByStatus', methods=["GET","POST"])
 def rfis_by_status():
-    # a variable to concatenate json strings
-    json_arr = []
-    headers = {'accept': 'application/json', 'content-type': 'application/json'}
-    # get all instances in the RFI system
-    resp=requests.get(pam_uri+'/services/rest/server/queries/processes/instances/variables/v_priority', auth=HTTPBasicAuth(pam_user,pam_pass), headers=headers).json()
-    instances = resp['process-instance']
-    for instance in instances:
-        resp1 = requests.get(pam_uri+'/services/rest/server/containers/RCM2_1.0.2/processes/instances/'+str(instance['process-instance-id'])+'/variables',auth=HTTPBasicAuth(pam_user,pam_pass),headers=headers).json()
-        json_arr.append(resp1)
-    return render_template('rfis_by_status.html', rfis=json_arr)
-
+    return kie_response('rfis_by_status.html') 
 
 @application.route('/rfis', methods=['GET','POST'])
 def rfis():
+    return kie_response('rfis.html')
+
+def kie_response(template):
     # a variable to concatenate json strings
     json_arr = []
     headers = {'accept': 'application/json', 'content-type': 'application/json'}
-    # get all instances in the RFI system
-    resp=requests.get(pam_uri+'/services/rest/server/queries/processes/instances/variables/v_priority', auth=HTTPBasicAuth(pam_user,pam_pass), headers=headers).json()
-    instances = resp['process-instance']
-    for instance in instances:
-        resp1 = requests.get(pam_uri+'/services/rest/server/containers/RCM2_1.0.2/processes/instances/'+str(instance['process-instance-id'])+'/variables',auth=HTTPBasicAuth(pam_user,pam_pass),headers=headers).json()
-        json_arr.append(resp1)
-    return render_template('rfis.html', rfis=json_arr)
+    try:
+        # get all instances in the RFI system
+        resp=requests.get(pam_uri+'/services/rest/server/queries/processes/instances/variables/v_priority', auth=HTTPBasicAuth(pam_user,pam_pass), headers=headers).json()
+        instances = resp['process-instance']
+        for instance in instances:
+            resp1 = requests.get(pam_uri+'/services/rest/server/containers/' + container_id + '/processes/instances/'+str(instance['process-instance-id'])+'/variables',auth=HTTPBasicAuth(pam_user,pam_pass),headers=headers).json()
+            json_arr.append(resp1)
+        return render_template(template, rfis=json_arr)
+    except json.decoder.JSONDecodeError as error:
+       msg = "It looks like the KIE server didn't provide a valid JSON response. It might be worth checking your config, here's the response we tried to parse:"
+       return render_template('500.html', error_msg=msg, raw_error=error.doc)
+
